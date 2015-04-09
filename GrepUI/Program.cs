c6a2115156b8da7407a52f31809d31a44bce8ef0
arek <test>
2015-04-09 22:17:52 +0200

    test

--- GrepUI/Program.cs
+++ GrepUI/Program.cs
@@ -17,7 +17,6 @@ namespace GrepUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
         }
     }
 }
