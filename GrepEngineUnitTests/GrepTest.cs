f2ed6ff49fde92553f85481a6c1c972c46cc3b61
marctheawesome <marek.wrobel08@gmail.com>
2015-04-10 15:10:35 +0200

    added grep engine test

---
+++ GrepEngineUnitTests/GrepTest.cs
@@ -0,0 +1,29 @@
+﻿using GrepEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrepEngineUnitTests
+{
+    [TestClass]
+    public class GrepTest
+    {
+        [TestMethod]
+        public void ShouldCountTokenOccurancesInSingleLine()
+        {
+            //Assign
+
+            var Grep = new Grep(new[]
+            {
+                "marxk", "szymon"
+            });
+
+            string token = "x";
+
+            //Act
+
+            int amount = Grep.CountTokenOccurancesInAllLines(token);
+
+            //Assert
+            Assert.AreEqual(1, amount);
+        }
+    }
+}
