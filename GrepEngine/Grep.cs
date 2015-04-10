6de24209b1c2bdbed86af302a37af65372950390
Szymon Bartnik <sbartnik@gmail.com>
2015-04-10 14:11:29 +0200

    Added grep interface.

---
+++ GrepEngine/Grep.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GrepEngine
+{
+    public class Grep : IGrep
+    {
+        public IEnumerable<string> InputData { get; private set; }
+
+        public Grep(IEnumerable<string> inputData)
+        {
+            InputData = inputData;
+        }
+
+        public GrepResult FindLinesContainingSpecifiedString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GrepEngine/IGrep.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GrepEngine
+{
+    public interface IGrep
+    {
+        GrepResult FindLinesContainingSpecifiedString();
+    }
+
+    public class GrepResult
+    {
+        public int OccurancesCount { get; private set; }
+        public IEnumerable<string> LinesContaining { get; private set; }
+    }
+}
