df0b77c2c0ab2d2d80ce999e4e0890e9f6ccfbb4
Maciej Dzianowicz <maciej.dzianowicz@itsilesia.com>
2015-04-10 17:19:56 +0200

    final console

--- GrepConsole/GrepConsoleProgram.cs
+++ GrepConsole/GrepConsoleProgram.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-
+using GrepEngine;
+using DataProvider;
 namespace GrepConsole
 {
-    class GrepConsoleProgram
+    public class GrepConsoleProgram
     {
+        private ArgsList argsList;
+
         public GrepConsoleProgram(ArgsList argsList)
         {
-            
+            this.argsList = argsList;
+        }
+
+        private Grep grep;
+
+        public void Run()
+        {
+            grep = new Grep(new Downloader().DownloadWebpageAsText(argsList.URL));
+            if (argsList.CountWords)
+            {
+                Console.WriteLine("Occurances of a given word in all lines:");
+                Console.WriteLine(grep.CountTokenOccurancesInAllLines(argsList.Word));
+            }
+            if (argsList.SearchWord)
+            {
+                Console.WriteLine("Lines where the given word occure:");
+                Console.WriteLine(grep.GetLinesContainingSpecifiedToken(argsList.Word));
+            }
+
+
         }
     }
 }
