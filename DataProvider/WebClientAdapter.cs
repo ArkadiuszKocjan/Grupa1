5b560dc3d1c37881aac456f0d9cdbde54d7d9d97
Maciej Dzianowicz <maciej.dzianowicz@itsilesia.com>
2015-04-10 15:26:01 +0200

    Added IWebClientInterface and Adapter

---
+++ DataProvider/WebClientAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider
+{
+    class WebClientAdapter : IWebClient
+    {
+        WebClient webClient;  
+        WebClientAdapter(WebClient webClient)
+        {
+            this.webClient = webClient;
+        }
+
+        public void DownloadFile(string address, string fileName)
+        {
+            webClient.DownloadFile(address,fileName);
+        }
+    }
+}
